a85b0688171c033c93251df10db9ac53c25957f4
Roxana.Mihailescu <roxana2302@yahoo.com>
2019-06-21 20:46:08 +0300

    Creare JS si style1

---
+++ JS/style1.js
@@ -0,0 +1,54 @@
+// var age = 15;
+// if(age <= 15 ani) {
+//   alert ("Primesti discount 25% reducere");
+// } else {
+// alert ("Nu primesti nicio reducere");
+// }
+// var age = 25;
+// if (age >= 15 ani && age < 25 Ani) {
+// alert ("Primesti reducere de 10%");
+// } else {
+//   alert ("Nu primesti nicio reducere");
+// }
+// if(age > 25 ani) {
+//   alert ("Nu primesti nicio reducere");
+// }
+
+// Prima parte practica//
+// var age = 15;
+// if (age <= 15 ani) {
+// alert ("Primesti discount 25% reducere");
+// } else if (age > 15 && age <= 25) {
+
+// alert ("Primesti reducere de 10%");
+// } else {
+// alert ("Nu primesti nicio reducere");
+// }
+// //Partea a doua practica//
+// var age = 20;
+// switch (age) {
+// case 15:
+// alert ("Primesti 25% reducere!");
+// break;
+// case 16:
+// alert ("Primesti 20% reducere!!!"); 
+// break; 
+// case 17:
+// alert ("Primesti 15% reducere!!!"); 
+// break;
+// case 18:
+// alert ("Primesti 10% reducere!!!"); 
+// break;
+// default:
+// alert ("Nu primesti nicio reducere!!!"); 
+// }
+// /Partea a treia de practica////
+//  var age =17;
+// //1//
+//  for (var i = 0; i < 100; i++) {
+// console.log(i);
+ }
+// //2//
+ for (var i = 0; i < 100; i = i+2) {
+  console.log(i);
+   }
