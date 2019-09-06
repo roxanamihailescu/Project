3e7cd1f712dd5f1bb17db5ee405b351d47ff7d4d
Roxana.Mihailescu <roxana2302@yahoo.com>
2019-09-06 15:56:28 +0300

    Practica to do -js

--- todoapp/todos.js
+++ todoapp/todos.js
@@ -1,5 +1,47 @@
-//this is an authorized route-token needed!//
-const todosUTL ="HTTP://localhost:3000/api/todos"
-function authorizedFetch(options ={}) {
-  
+// //this is an authorized route-token needed!// laora// 
+// const todosUTL ="HTTP://localhost:3000/api/todos"
+// function authorizedFetch(url, options ={ 
+//     method: "GET"
+// }) {
+//   return fetch (url, {
+//       ...options,
+//       headers: {
+//           Authorization: 'Bearer ${sesionStorage.
+//         }
+//           'Content-Type':'application/json'.get DataTransferItem('token')}
+//           authorizedFetch(URL, {
+//               method:'POST',
+//               body: JSON.stringify({...})
+//           })
+//       }
+//   })
+// }
+// .GET@/api/todos & display all the DataTransferItemList
+
+// // //add an input and create a button
+// // post @ /api/todos to create a new todo
+// // ///
+//practice//
+var objPeople= [
+    {
+        Email: "alex@yahoo.com",
+        password: "codify"
+    },
+    { 
+        Email: "bo@yahoo.com",
+        password: "nice"
+
+    },
+    {
+        Email: "roxy@yahoo.com",
+        password: "yare"
+    }
+
+    }
+]
+function getInfo() {
+    var email = document.getElementById(Email).value
+    var password = document.getElementById(password).value
+    console.log("you`re email is" + email + "and your password is" + password)
+
 }
